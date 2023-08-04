78f19a7eecac85b6f3efe0199e093227a87428aa
jwooklee <jaewook04@gmail.com>
2023-08-05 01:28:40 +0900

    feat: 메인페이지 초안 구현

--- src/components/layout/common.js
+++ src/components/layout/common.js
@@ -3,8 +3,11 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  padding: 0px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
 `;
 
 export const Button = styled.button`
@@ -19,4 +22,72 @@ export const Button = styled.button`
 export const Title = styled.div`
   font-size: 30px;
   margin: 20px;
+`;
+
+export const NavCategory = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  left: 80px;
+  top: 20px;
+`;
+
+export const Logo = styled.div`
+  position: absolute;
+  left: 6%;
+  top: 20%;
+  color: #010d09;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 38px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+`;
+
+export const NavCosmetic = styled.div`
+  position: absolute;
+  left: 30%;
+  top: 30%;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #010d09;
+`;
+
+export const NavINCIpedia = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 30%;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #010d09;
+`;
+
+export const SearchBar = styled.div`
+  display: flex;
+`;
+
+export const CosmeticImgList = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: flex-start;
+  padding: 0px;
+  gap: 20px 21px;
 `;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,83 @@
 import React from "react";
 import styled from "styled-components";
-import { Wrapper } from "../components/layout/common";
+import {
+  Wrapper,
+  Logo,
+  NavCosmetic,
+  NavINCIpedia,
+} from "../components/layout/common";
+import { Nav, Section, Footer } from "../components/layout/Layout";
 
 const Home = () => {
   return (
     <Wrapper>
-      <Header>헤더</Header>
-      <Body>바디</Body>
-      <Footer>푸터</Footer>
+      <Nav>
+        <Logo>Kisse</Logo>
+        <NavCosmetic>All Category</NavCosmetic>
+        <NavINCIpedia>INCIpedia</NavINCIpedia>
+      </Nav>
+      <Section>
+        <About></About>
+        <h2>
+          <br />
+          Categories
+        </h2>
+        <ImageSection>
+          <CosmeticImage />
+          <CosmeticImage />
+          <CosmeticImage />
+          <CosmeticImage />
+          <CosmeticImage />
+          <CosmeticImage />
+          <CosmeticImage />
+          <CosmeticImage />
+        </ImageSection>
+      </Section>
+      <Footer>
+        <FooterStyle>
+          <h3>Contact Us</h3>
+          <div>Tel : 02-1234-5678</div>
+          <div>
+            Mail : ouremail@gmail.com
+            <br />
+            <br />
+          </div>
+          <h3>Copyright</h3>
+          <div>Image by valuavitaly on Freepik</div>
+        </FooterStyle>
+      </Footer>
     </Wrapper>
   );
 };
 
 export default Home;
 
-const Header = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 10vh;
-  width: 100vw;
+const FooterStyle = styled.div`
+  margin-left: 6%;
 `;
 
-const Body = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 80vh;
-  width: 100vw;
+const About = styled.div`
+  width: 90%;
+  height: 30%;
+  background: url(<path-to-image>),
+    lightgray 0px -281.646px / 100% 284.444% no-repeat;
 `;
 
-const Footer = styled.div`
+const ImageSection = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 20vh;
-  width: 100vw;
-  background-color: #9dc8c8;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: space-evenly;
+  align-content: space-between;
+  width: 90%;
+  height: 50%;
+  margin-top: 0;
+`;
+
+const CosmeticImage = styled.div`
+  width: 23.8%;
+  height: 48%;
+  background: url(<path-to-image>),
+    lightgray 0px -281.646px / 100% 284.444% no-repeat;
 `;
