99884c3435ccefee414a5d619c52d9fc2b0b8565
jwooklee <jaewook04@gmail.com>
2023-08-06 13:00:17 +0900

    feat: 메인페이지 구현 완료

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,31 +9,57 @@ import {
   Logo,
   NavCosmetic,
   NavINCIpedia,
+  SearchBar,
+  SearchButton,
+  MyPageButton,
 } from "../components/layout/Layout";
+
+import searchIcon from "../img/searchIcon.png";
+import myPageIcon from "../img/myPageIcon.png";
+
+// import Moisturizer from "../img/Moisturizer.png";
+// import SunCare from "../img/SunCare.png";
+// import Mask from "../img/Mask.png";
+// import Eye from "../img/Eye.png";
+// import Face from "../img/Face.png";
+// import Lip from "../img/Lip.png";
+// import Cleanser from "../img/Cleanser.png";
+// import AllProducts from "../img/AllProducts.png";
 
 const Home = () => {
   return (
     <Wrapper>
       <Nav>
-        <Logo>Kisse</Logo>
+        <Logo>VEBE</Logo>
         <NavCosmetic>All Category</NavCosmetic>
         <NavINCIpedia>INCIpedia</NavINCIpedia>
+        <SearchBar>
+          <input
+            type="text"
+            placeholder="Search"
+            style={{ border: "none", height: "1.2rem" }}
+          ></input>
+          <SearchButton src={searchIcon} />
+        </SearchBar>
+        <MyPageButton src={myPageIcon} />
       </Nav>
       <Section>
         <About></About>
-        <h2>
-          <br />
-          Categories
-        </h2>
+        <Category>
+          <h2>
+            <br />
+            Categories
+          </h2>
+        </Category>
         <ImageSection>
-          <CosmeticImage />
-          <CosmeticImage />
-          <CosmeticImage />
-          <CosmeticImage />
-          <CosmeticImage />
-          <CosmeticImage />
-          <CosmeticImage />
-          <CosmeticImage />
+          <CosmeticImage></CosmeticImage>
+          <CosmeticImage></CosmeticImage>
+          <CosmeticImage></CosmeticImage>
+          <CosmeticImage></CosmeticImage>
+          <CosmeticImage></CosmeticImage>
+          <CosmeticImage></CosmeticImage>
+          <CosmeticImage></CosmeticImage>
+          <CosmeticImage></CosmeticImage>
         </ImageSection>
       </Section>
       <Footer>
@@ -74,9 +100,18 @@ const ImageSection = styled.div`
   margin-top: 0;
 `;
 
-const CosmeticImage = styled.div`
+const CosmeticImage = styled.button`
   width: 23.8%;
   height: 48%;
-  background: url(<path-to-image>),
-    lightgray 0px -281.646px / 100% 284.444% no-repeat;
+  background: url(), lightgray 0px -281.646px / 100% 284.444% no-repeat;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.4);
+    color: white;
+  }
+  border: none;
+`;
+
+const Category = styled.div`
+  align-self: flex-start;
+  margin-left: 5%;
 `;
