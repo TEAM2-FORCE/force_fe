ec8e857d4b9b5da06050f32af72ba2575af9a8fc
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-08-07 17:19:38 +0900

    refactor: 충돌 해결 시도

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import MyPage from "./pages/MyPage";
+import ItemDetail from "./pages/ItemDetail";
 import Moisturizer from "./pages/search/Moisturizer";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/myPage" element={<MyPage />} />
+        <Route path="/itemdetail" element={<ItemDetail />} />
         <Route path="/moisturizer" element={<Moisturizer />} />
       </Routes>
     </BrowserRouter>
