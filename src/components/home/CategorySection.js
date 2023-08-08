20a102ea2ba496643fdb43c30a0e95dd9321506b
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-08-08 20:17:27 +0900

    refactor: Home 코드 정리 및 CategorySection 파일로 내용 이동

---
+++ src/components/home/CategorySection.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { styled } from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import moisturizer from "../../img/Moisturizer.png";
+import suncare from "../../img/SunCare.png";
+import mask from "../../img/Mask.png";
+import eye from "../../img/Eye.png";
+import face from "../../img/Face.png";
+import lip from "../../img/Lip.png";
+import cleanser from "../../img/Cleanser.png";
+import all from "../../img/AllProducts.png";
+
+const CategorySection = ({whereTo, name}) => {
+  const navigate = useNavigate();
+  const goto = (where) => {
+    navigate(`/${where}`);
+  };
+  const imageMapping = {
+    moisturizer: moisturizer,
+    suncare: suncare,
+    mask: mask,
+    eye: eye,
+    face: face,
+    lip: lip,
+    cleanser: cleanser,
+    all: all,
+  };
+  return (
+    <>
+    <ImageWrapper onClick={() => goto(whereTo)}>
+    <Image src={imageMapping[whereTo]} alt="Image" />
+      <TextContainer>
+        <h3>{name}</h3>
+      </TextContainer>
+    </ImageWrapper>
+    </>
+  )
+}
+
+export default CategorySection;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  display: inline-block; //인라인 블록을 해주어야 요소들끼리 겹치게 만들어줄 수 있음!!
+  width: 23.8%;
+  height: 48%;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  border: none;
+  &:hover {
+    cursor: pointer;
+    z-index: 0;
+  }
+`;
+
+const TextContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.4);
+  color: white;
+  &:hover {
+    z-index: 2;
+  }
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,25 +4,10 @@ import { Wrapper } from "../components/layout/common";
 import { Section } from "../components/layout/Layout";
 import Nav from "../components/layout/Nav";
 import Footer from "../components/layout/Footer";
-
 import about from "../img/about.png";
-import moisturizer from "../img/Moisturizer.png";
-import sunCare from "../img/SunCare.png";
-import mask from "../img/Mask.png";
-import eye from "../img/Eye.png";
-import face from "../img/Face.png";
-import lip from "../img/Lip.png";
-import cleanser from "../img/Cleanser.png";
-import allProducts from "../img/AllProducts.png";
-
-import { useNavigate } from "react-router-dom";
+import CategorySection from "../components/home/CategorySection";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const goto = (where) => {
-    navigate(`/${where}`);
-  };
-
   return (
     <Wrapper>
       <Nav></Nav>
@@ -37,54 +22,14 @@ const Home = () => {
           </h2>
         </Category>
         <ImageSection>
-          <ImageWrapper onClick={() => goto("moisturizer")}>
-            <Image src={moisturizer} alt="Image" />
-            <TextContainer>
-              <h3>Moisturizer</h3>
-            </TextContainer>
-          </ImageWrapper>
-          <ImageWrapper onClick={() => goto("suncare")}>
-            <Image src={sunCare} alt="Image" />
-            <TextContainer>
-              <h3>Sun Care</h3>
-            </TextContainer>
-          </ImageWrapper>
-          <ImageWrapper onClick={() => goto("cleanser")}>
-            <Image src={cleanser} alt="Image" />
-            <TextContainer>
-              <h3>Cleanser</h3>
-            </TextContainer>
-          </ImageWrapper>
-          <ImageWrapper onClick={() => goto("masks")}>
-            <Image src={mask} alt="Image" />
-            <TextContainer>
-              <h3>Mask</h3>
-            </TextContainer>
-          </ImageWrapper>
-          <ImageWrapper onClick={() => goto("face")}>
-            <Image src={face} alt="Image" />
-            <TextContainer>
-              <h3>Face</h3>
-            </TextContainer>
-          </ImageWrapper>
-          <ImageWrapper onClick={() => goto("eye")}>
-            <Image src={eye} alt="Image" />
-            <TextContainer>
-              <h3>Eye</h3>
-            </TextContainer>
-          </ImageWrapper>
-          <ImageWrapper onClick={() => goto("lip")}>
-            <Image src={lip} alt="Image" />
-            <TextContainer>
-              <h3>Lip</h3>
-            </TextContainer>
-          </ImageWrapper>
-          <ImageWrapper onClick={() => goto("all")}>
-            <Image src={allProducts} alt="Image" />
-            <TextContainer>
-              <h3>All Products</h3>
-            </TextContainer>
-          </ImageWrapper>
+          <CategorySection whereTo="moisturizer" name="Moisturizer"/>
+          <CategorySection whereTo="suncare" name="Sun Care"/>
+          <CategorySection whereTo="face" name="Face"/>
+          <CategorySection whereTo="eye" name="Eye"/>
+          <CategorySection whereTo="lip" name="Lip"/>
+          <CategorySection whereTo="cleanser" name="Cleanser"/>
+          <CategorySection whereTo="mask" name="Masks"/>
+          <CategorySection whereTo="all" name="All"/>
         </ImageSection>
       </Section>
       <Footer></Footer>
@@ -117,49 +62,6 @@ const ImageSection = styled.div`
   width: 90%;
   height: 50%;
   margin-top: 0;
-`;
-
-const ImageWrapper = styled.div`
-  position: relative;
-  display: inline-block; //인라인 블록을 해주어야 요소들끼리 겹치게 만들어줄 수 있음!!
-  width: 23.8%;
-  height: 48%;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: none;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  border: none;
-  &:hover {
-    cursor: pointer;
-    z-index: 0;
-  }
-`;
-
-const TextContainer = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.4);
-  color: white;
-  &:hover {
-    z-index: 2;
-  }
 `;
 
 const Category = styled.div`
