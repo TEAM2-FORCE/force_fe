19c6d3a0513da17145ec811cc54c5d7f640f57a4
jwooklee <jaewook04@gmail.com>
2023-08-11 05:58:40 +0900

    feat: 성분 api 컴포넌트 구성 완료

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import { FooterContainer, FooterStyle } from './Layout'
+import React from "react";
+import { FooterContainer, FooterStyle } from "./Layout";
+import { getPriceHighToLow } from "../../apis/Item";
 
 const Footer = () => {
+  const example = async () => {
+    const response = await getPriceHighToLow(2);
+    console.log(response);
+  };
   return (
-    <FooterContainer>
-        <FooterStyle>
-          <h3>Contact Us</h3>
-          <div>Tel : 02-1234-5678</div>
-          <div>
-            Mail : ouremail@gmail.com
-            <br />
-            <br />
-          </div>
-          <h3>Copyright</h3>
-          <div>Image by valuavitaly on Freepik</div>
-        </FooterStyle>
+    <FooterContainer onClick={example}>
+      <FooterStyle>
+        <h3>Contact Us</h3>
+        <div>Tel : 02-1234-5678</div>
+        <div>
+          Mail : ouremail@gmail.com
+          <br />
+          <br />
+        </div>
+        <h3>Copyright</h3>
+        <div>Image by valuavitaly on Freepik</div>
+      </FooterStyle>
     </FooterContainer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
